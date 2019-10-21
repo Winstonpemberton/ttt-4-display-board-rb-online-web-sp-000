d7722f293c9ef9ee86ef25accb37c3f796369c77
Winston Pemberton <winstonpemberton@gmail.com>
2019-10-21 22:38:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board (board)
-  puts " #{board} | #{board} | #{board} "
-  puts "-----------"
-  puts " #{board} | #{board} | #{board} "
-  puts "-----------"
-  puts " #{board} | #{board} | #{board} "
+def display_board (board = " ")
+  rows = [" #{board} | #{board} | #{board} ","-----------"," #{board} | #{board} | #{board} ","-----------"," #{board} | #{board} | #{board} "]
+  puts rows[0]
+  puts rows[1]
+  puts rows[2]
+  puts rows[3]
+  puts rows[4]
 end
